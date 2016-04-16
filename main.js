3c92822c8049c0f45c27831f3b27f124015941ed
Brian Kim <byk.briankim@gmail.com>
2016-04-16 00:27:17 -0700

    initial push

---
+++ main.js
@@ -0,0 +1,8 @@
+// scrape twitter to start
+
+//
+console.log('Scraping @theflightdeal tweets');
+
+console.log('Searching array of tweets for San Francisco'); // making sure it has no duplicates
+
+console.log('New entry found, send email to:');
